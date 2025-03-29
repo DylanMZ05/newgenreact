4d687cf77c59e9cd7632473a3fefa127bf156be2
DylanMZ <dylanmazurzgainer@gmail.com>
2025-03-29 08:55:53 -0300

    a

--- src/pages/Contact/ContactRedirect.tsx
+++ src/pages/Contact/ContactRedirect.tsx
@@ -6,13 +6,13 @@ const ContactRedirect = () => {
       <div className="bg-black/50 w-full h-full min-h-screen flex items-center justify-center px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-6xl">
           {[
-            { title: "Escribenos!", desc: "Esta es la primera tarjeta", to: "/ruta1" },
-            { title: "Tarjeta 2", desc: "Esta es la segunda tarjeta", to: "/ruta2" },
+            { title: "Contact Us", desc: "Esta es la primera tarjeta", to: "/ruta1" },
+            { title: "Free Quote", desc: "Esta es la segunda tarjeta", to: "/ruta2" },
             { title: "Tarjeta 3", desc: "Esta es la tercera tarjeta", to: "/ruta3" },
           ].map((card, index) => (
             <div
               key={index}
-              className="bg-white rounded-2xl shadow-md p-6 flex flex-col justify-between"
+              className="bg-white rounded-2xl shadow-md text-center p-6 flex flex-col justify-between"
             >
               <div>
                 <h2 className="text-xl font-bold mb-2">{card.title}</h2>
@@ -20,9 +20,9 @@ const ContactRedirect = () => {
               </div>
               <Link
                 to={card.to}
-                className="mt-auto inline-block bg-blue-600 text-white text-sm px-4 py-2 rounded hover:bg-blue-700 transition"
+                className="mt-auto mx-auto w-30 bg-blue-600 text-white text-sm text-center px-4 py-2 rounded-full hover:bg-blue-700 transition"
               >
-                Ir a {card.title}
+                {card.title}
               </Link>
             </div>
           ))}
