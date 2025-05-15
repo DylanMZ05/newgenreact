b760f44084f8df4459357605607f29fe8abfd59a
DylanMZ <dylanmazurzgainer@gmail.com>
2025-05-15 14:38:32 -0300

    a

--- src/components/ContractorCard.tsx
+++ src/components/ContractorCard.tsx
@@ -8,8 +8,8 @@ const ContractorCard: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-3 bg-gradient-to-b from-red-800 to-purple-800 text-center text-white p-6 rounded-lg shadow-lg w-full mt-6 md:mt-10 md:h-100 md:max-w-md">
       <p className="text-2xl font-bold">ARE YOU A CONTRACTOR?</p>
-      <p className="text-xl text-white/80">We’re always looking for skilled professionals.</p>
-      <p className="text-2xl font-bold">Let’s build something great together!</p>
+      <p className="text-xl text-white/80">We are always open to teamwork.</p>
+      <p className="text-xl font-bold">If you have a project at hand, you need help, and you want to delegate that outdoor space to professionals.</p>
       <button
         onClick={redirectToForm}
         className="bg-orange-500 text-white px-4 py-2 mt-4 rounded-full font-semibold hover:bg-orange-600 transition-all"
